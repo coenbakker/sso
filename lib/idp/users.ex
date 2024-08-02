022ce7abb18cfef54aa3cb35b5fc21e19e565957
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-08-02 18:56:02 +0200

    Add docs to users context

--- lib/idp/users.ex
+++ lib/idp/users.ex
@@ -1,7 +1,14 @@
 defmodule Idp.Users do
+  @moduledoc """
+  Module responsible for exposing the context functions related to managing users.
+  """
+
   alias Idp.Repo
   alias Idp.Users.User
 
+  @doc """
+  Registers a user with the given attributes.
+  """
   @spec register_user(attrs :: map()) :: {:ok, User.t()} | {:error, Ecto.Changeset.t()}
   def register_user(attrs) do
     %User{}
