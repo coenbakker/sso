ebe57407391c44ab5eeea862a20445a92ed87bb7
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-08-09 11:53:04 +0200

    Fix auth controller test implementation

--- example_client/test/example_client_web/controllers/auth_controller_test.exs
+++ example_client/test/example_client_web/controllers/auth_controller_test.exs
@@ -1,32 +1,25 @@
 defmodule ExampleClientWeb.AuthControllerTest do
   use ExampleClientWeb.ConnCase, async: true
-  alias ExampleClientWeb.JokenTestUtils
+  import ExampleClientWeb.JokenTestUtils
 
-  @sso Application.compile_env(:example_client, :sso)
-  @login_uri Keyword.get(@sso, :domain) <> Keyword.get(@sso, :endpoint)
+  test "redirects to given resource path", %{conn: conn} do
+    valid_resource_token = build_resource_token!("/private")
+    conn = get(conn, "/callback?access_token=token&resource=#{valid_resource_token}")
+    assert redirected_to(conn) == "/private"
 
-  test "valid access token grants access to /private", %{conn: conn} do
-    valid_access_token = JokenTestUtils.build_access_token!()
-    valid_resource_token = JokenTestUtils.build_resource_token!("/private")
-
-    conn =
-      get(conn, "/callback?access_token=#{valid_access_token}&resource=#{valid_resource_token}")
-
-    assert "/private" = redir_path = redirected_to(conn)
-
-    conn = get(recycle(conn), redir_path)
-    assert html_response(conn, 200) =~ "The Private Page"
+    valid_resource_token = build_resource_token!("/other-path")
+    conn = get(conn, "/callback?access_token=token&resource=#{valid_resource_token}")
+    assert redirected_to(conn) == "/other-path"
   end
 
-  test "invalid access token redirects to SSO login endpoint", %{conn: conn} do
-    valid_resource_token = JokenTestUtils.build_resource_token!("/private")
+  test "puts the access token in a request header", %{conn: conn} do
+    valid_access_token = build_access_token!()
+    valid_resource_token = build_resource_token!("/private")
 
     conn =
-      get(conn, "/callback?access_token=invalid_access_token&resource=#{valid_resource_token}")
+      conn
+      |> get("/callback?access_token=#{valid_access_token}&resource=#{valid_resource_token}")
 
-    assert "/private" = redir_path = redirected_to(conn)
-
-    conn = get(recycle(conn), redir_path)
-    assert redirected_to(conn) == @login_uri
+    assert get_req_header(conn, "authorization") == ["Bearer #{valid_access_token}"]
   end
 end
